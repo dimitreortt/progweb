719f4228697e76679fe0c2c147caff0b98278da6
dimitreortt <51975072+dimitreortt@users.noreply.github.com>
2019-11-12 15:18:30 -0400

    Update VerificaLogin.php

--- Prototype/VerificaLogin.php
+++ Prototype/VerificaLogin.php
@@ -6,8 +6,9 @@ include_once("Database.php");
 
 echo $_POST["usuario"] . "<br>";
 echo $_POST["senha"]. "<br>";
+$username = $_POST["usuario"];
 
-$res = $db->query('SELECT * FROM Usuarios');
+$res = $db->query('SELECT * FROM Usuarios where usuario=`'.$username.'`');
 
 while ($row = $res->fetchArray()) {
     echo "{$row['nome']} {$row['email']} {$row['password']}" .  "<br>";
